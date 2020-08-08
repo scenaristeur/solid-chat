da273c3d896a82954793cc90766eced773edaecd
David <scenaristeur@gmail.com>
2020-08-08 21:58:10 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.95ee86a489c895458900e060b7fbf152.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2f34a66bcf8caa4140",
+    "url": "/css/app.615ddd25.css"
+  },
+  {
+    "revision": "062828921ab92981a5e3",
+    "url": "/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "a9750865d99b9f539fa6a972bbf34bd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf976bf458dc9e679d0",
+    "url": "/js/about.f52b7c52.js"
+  },
+  {
+    "revision": "1b2f34a66bcf8caa4140",
+    "url": "/js/app.c6273eb1.js"
+  },
+  {
+    "revision": "062828921ab92981a5e3",
+    "url": "/js/chunk-vendors.cc4aa6c9.js"
+  },
+  {
+    "revision": "3d8c91fa777f1d8fcecda2368dd719fc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83dfc112ffad3b778c6f82c9147f94b8",
+    "url": "/vendor/rdflib.min.js"
+  },
+  {
+    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
+    "url": "/vendor/solid-file-client.bundle.js"
+  },
+  {
+    "revision": "942b236b1c9c4b0d70bf8fadc1f38310",
+    "url": "/vendor/solid-query-ldflex.bundle.js"
+  },
+  {
+    "revision": "251b053187b51004112ec6c53a615781",
+    "url": "/vendor/solid-query-ldflex.rdflib.js"
+  }
+]);
